03ce091aba527253fc37a09fb71bcd6c61a3db92
Patrick Ratcliff <pratcliff5@gmail.com>
2023-10-25 13:55:10 -0600

    UI: debug Lottie Player issue

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
 import { AuthProvider } from "./context/AuthProvider";
 import { ToastContainer } from 'react-toastify';
 
+import App from './App';
+
+import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import Lottie from 'react-lottie';
+import { Player, Controls } from '@lottiefiles/react-lottie-player';
 import animationData from '../images/landing-gears.json';
+
 import '../styles/Landing.css';
 
 export default function Landing() {
-  // Configure Lottie options
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-  };
 
   return (
     <div className="landing-card mt-3">
       <h1>Welcome to Repair Shop</h1>
       <p>Sign in to get started!</p>
-      <Lottie options={defaultOptions} height={200} width={200} />
+      <Player
+        autoplay
+        loop
+        src={animationData}
+        style={{ height: '30vw', width: '30vw' }}
+      >
+        <Controls visible={false} buttons={['play', 'repeat', 'frame', 'debug']} />
+      </Player>
     </div>
   );
 }
